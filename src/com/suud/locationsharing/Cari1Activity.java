ba3da872a4c86e35a7684d497dfce92e7c41174d
Neoreeprast <neoreeprast@gmail.com>
2013-08-15 11:10:32 +0700

    add log to Cari1Activity

--- src/com/suud/locationsharing/Cari1Activity.java
+++ src/com/suud/locationsharing/Cari1Activity.java
@@ -3,6 +3,7 @@ package com.suud.locationsharing;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class Cari1Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cari1);
+		Log.d("Cari1Activity", "onCreateActivity");
 		 Button btn=(Button)findViewById(R.id.btnkomentar);
 		    btn.setOnClickListener(new View.OnClickListener() {
 				
